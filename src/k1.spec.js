54c9be6da95203b6938b031d90ad0a2327f3c69e
Stan Muszel <stanislaw.muszel@gmail.com>
2019-06-02 07:41:51 +0200

    move katas

--- src/k1.spec.js
+++
@@ -1,8 +0,0 @@
-const t = require('../util');
-
-t('Adding 2 values', t => {
-    t(1 + 2, 3);
-    t(1 + 3, 4);
-    t(1 + 2, 4);
-    t(2 + 3, 5);
-});
--- util.js
+++ util.js
@@ -1,19 +1,46 @@
-const { isDeepStrictEqual } = require('util');
+const cnc = require('concordance');
+const chalk = require('chalk').default;
+
+const stackTraceParser = stackTrace => {
+    const chunks = stackTrace.split(/\n/g);
+    const caller = chunks[2].split('at ')[1].split(':');
+    const callerFilename = caller[0] + ':' + caller[1];
+    const callerLine = +caller[2];
+
+    return { callerFilename, callerLine };
+};
 
 module.exports = (title, t) => {
-    let i = 0;
     const comparator = (a, b) => {
-        const pass = isDeepStrictEqual(a, b);
+        const pass = cnc.compare(a, b).pass;
 
         if (!pass) {
-            const cnc = require('concordance');
-            const chalk = require('chalk').default;
             const diff = cnc.diff(a, b);
-            console.log(chalk.red(title), `[${i}]`);
-            console.log(diff);
+            const { callerLine } = stackTraceParser(new Error().stack);
+            const out = JSON.stringify({ callerLine, diff });
+
+            console.log(out);
         }
-        i++;
     };
 
     t(comparator);
 };
+
+// const fs = require('fs');
+// const src = fs
+//     .readFileSync(ss[0] + ':' + ss[1])
+//     .toString()
+//     .split('\n');
+// const startIx = src.findIndex(line => line.includes(title));
+// const endIx =
+//     src.slice(startIx, src.length).findIndex(line => {
+//         return line === '});\r';
+//     }) + startIx;
+// const lines = src.slice(startIx, endIx + 1);
+// const failIx = +ss[2] - startIx - 1;
+// console.log(failIx);
+// lines[failIx] = chalk.bgRedBright(lines[failIx]);
+// console.log('\n');
+// console.log(lines.join('\n'));
+// console.log('\n');
+// console.log(diff);
